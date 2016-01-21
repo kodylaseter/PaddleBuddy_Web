484eafe0e5dc031081c3bc572b81521b86f76fdd
Kody Laseter <kodylaseter1@gmail.com>
2016-01-21 12:50:37 -0500

    working on angular-ng-maps stuff

--- public/core.js
+++ public/core.js
@@ -1,12 +1,24 @@
-var pbWeb = angular.module('pbWeb', []);
+var pbWeb = angular.module('pbWeb', ['uiGmapgoogle-maps']);
+    //.config(function(uiGmapGoogleMapApiProvider) {
+    //    uiGmapGoogleMapApiProvider.configure({
+    //        key: 'AIzaSyBIuCKiNReJwTU4frhP3ndPyvdrZt60pJU',
+    //        //libraries: 'weather,geometry,visualization'
+    //    });
+    //});
 
 function mainController($scope, $http) {
     $scope.test = "blah";
+    $scope.map = {
+        center: {
+            latitude: 45,
+            longitude:-73
+        },
+        zoom: 8
+    }
 
     $http.get('/api/points')
         .success(function(data) {
             $scope.points = data;
-            console.log(data);
         })
         .error(function(data) {
             console.log('Error: ' + data);
@@ -25,5 +37,6 @@ function mainController($scope, $http) {
                 console.log('Error: ');
             });
         console.log('add point called');
-    }
+    };
+
 }
