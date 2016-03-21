965f8862ace1cdc5e8e7444e8b204563c75450ed
Kody Laseter <kodylaseter1@gmail.com>
2016-03-21 17:37:50 -0400

    fixed dbmanager clear

--- dbmanager.js
+++ dbmanager.js
@@ -1,13 +1,34 @@
 /**
  * Created by Kody on 3/21/2016.
  */
+var mysql = require('mysql');
 
-var clearDB = function () {
+var connection = mysql.createConnection({
+    host: 'www.db4free.net',
+    user: 'kodylaseter',
+    password: 'password',
+    database: 'pb_test'
+});
+
+var clearDB = function(){
+    console.log('clear');
     var tables_to_clear = ['link', 'point', 'river'];
     for (var i = 0; i < tables_to_clear.length; i++) {
         var tableName = tables_to_clear[i];
-        connection.query('TRUNCATE TABLE pb_test.' + tableName, function(error) {
-            if (error) console.log(error);
-        })
+        (function(i) {
+            connection.query('TRUNCATE TABLE pb_test.' + tableName, function(error) {
+                if (error) console.log(error);
+                else {
+                    if (i == tables_to_clear.length - 1) {
+                        console.log('Finished!');
+                        process.exit();
+                    }
+                }
+            });
+        })(i);
     }
 };
+
+process.argv.forEach(function (val) {
+    if (val == 'clear') clearDB();
+});
